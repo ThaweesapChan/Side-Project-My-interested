e59a156ce698d97ff772edd3d2d7cf3688ef15a5
thaweesap chantasathanont <thaweesap@192.168.1.44>
2024-10-16 15:13:43 +0700

    setting project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section>
+        <h1 className="border  bg-blue-950">My First React App on Vite</h1>
+      </section>
+    </>
+  );
+}
+export default App;
